55cbdc3b3f09d3d1aa39d3d0778dd6c753f4eb12
Abasolo <rodulfo.jr.abasolo@accenture.com>
2024-10-13 23:20:59 +0800

    added conditional all numbers of the username

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -1,6 +1,7 @@
 import { HtmlParser } from '@angular/compiler';
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import test from 'node:test';
 
 
 
@@ -37,29 +38,43 @@ export class SignUpComponent {
     //checking the characters of the username
       const finalUserValue: string = userLengthVal[this.arr.length - 1]
 
+    
+
       const finalUserValueLength: number = finalUserValue.length
 
       const numerics: string[] = ["0","1","2","3","4","5","6","7","8","9"]
       // const numericss: number[] = [0,1,2,3,4,5,6,7,8,9]
       let incrementTest: number = 0;
+      let testA: number = 0;
       const testings: boolean = /\s/.test(finalUserValue);
       if(finalUserValueLength == this.userNameLength && testings === false) {
-        numerics.forEach((e) => {   
-          const test = finalUserValue.includes(e,0)
-          // console.log(test)
-          if(test) {
-            incrementTest++
-            if(incrementTest === 3) {
-              console.log("valid username")
+        for(let i = 0; i < finalUserValue.length; i++) {
+            const stringVal: string = finalUserValue[i]
+            const tests = numerics.includes(stringVal, 0)
+            if(tests) {
+              testA++
+              if(testA === 6) {
+                console.log("there should be atleast 3 alphabetical characters for the username")
+              }
+            } else {
+              numerics.forEach((e) => {   
+                const test = finalUserValue.includes(e,0)
+                // console.log(test)
+                if(test) {
+                  incrementTest++
+                  if(incrementTest === 3) {
+                    console.log("valid username")
+                  }
+                } 
+              })
             }
-          } 
-        })
+        }
         if(incrementTest < 3) {
           console.log("Username should have 3 numbers")
         }
 
       } else {
-        console.log("Username should 6 characters long without space character")
+        console.log("Username should 6 characters long or without space character")
       }
     }
 }
