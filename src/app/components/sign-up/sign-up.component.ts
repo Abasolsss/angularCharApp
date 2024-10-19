66350c9c83ae47218048f46722024b3fd6ca47a9
Abasolo <rodulfo.jr.abasolo@accenture.com>
2024-10-19 19:59:10 +0800

    new directives added

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -1,11 +1,11 @@
 import { HtmlParser } from '@angular/compiler';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,ReactiveFormsModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.scss',
 })
@@ -16,13 +16,19 @@ export class SignUpComponent {
   userFirstName: string = '';
   userLastName: string = '';
 
+  userInfo = new FormGroup({
+    userName: new FormControl(''),
+    firstName: new FormControl(''),
+    lastName: new FormControl('')
+  })
+
   submitFunct() {
     //username should have 6 letters
     // username shouldnt have uppercase
     // 3 letters and 3 numbers
     // 6 letters and 6 numbers = error
-
-    const userName: string = this.inputUserName;
+    const formValue = this.userInfo.value
+    const userName: string = formValue.userName!
     const userFirstName: string = this.userFirstName;
     const regExUppercase: RegExp = /([A-Z])/g;
     const regExNumber: RegExp = /([0-9])/g;
@@ -37,24 +43,21 @@ export class SignUpComponent {
     const numberLength = userNameNumbercheck?.length as number;
     const whiteSpaceCheck: boolean = /\s/.test(userName);
 
-    // if (
-    //   numberLength === 3 &&
-    //   stringLength === 3 &&
-    //   userUppercaseCheck === false &&
-    //   whiteSpaceCheck === false
-    // ) {
-    //   console.log('valid username');
-    //   if (this.userFirstName.length === 6) {
-    //     console.log('valid firstname');
-    //   }
-    // } else {
-    //   console.log(
-    //     'Username should have 3 numbers and 3 letters wtih no uppercase and no space'
-    //   );
-    // }
-
-    const test = testReg.test(userFirstName);
-
-    console.log(test);
+    if (
+      numberLength === 3 &&
+      stringLength === 3 &&
+      userUppercaseCheck === false &&
+      whiteSpaceCheck === false
+    ) {
+      console.log('valid username');
+      if (this.userFirstName.length === 6) {
+        console.log('valid firstname');
+      }
+    } else {
+      console.log(
+        'Username should have 3 numbers and 3 letters wtih no uppercase and no space'
+      );
+    }
+    
   }
 }
