1679370ea65edf365cfa8ef1f52b184c9e95a6a9
Abasolo <rodulfo.jr.abasolo@accenture.com>
2024-10-19 21:19:54 +0800

    regex for firstname and lastname added

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -2,6 +2,14 @@ import { HtmlParser } from '@angular/compiler';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+
+interface UserInfo {
+  userName: string,
+  firstName: string,
+  lastName: string
+}
+
+
 @Component({
   selector: 'app-sign-up',
   standalone: true,
@@ -12,9 +20,6 @@ import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angul
 
 //username should have 3 letters, 3 numbers
 export class SignUpComponent {
-  inputUserName: string = '';
-  userFirstName: string = '';
-  userLastName: string = '';
 
   userInfo = new FormGroup({
     userName: new FormControl(''),
@@ -27,22 +32,27 @@ export class SignUpComponent {
     // username shouldnt have uppercase
     // 3 letters and 3 numbers
     // 6 letters and 6 numbers = error
-    const formValue = this.userInfo.value
-    const userName: string = formValue.userName!
-    const userFirstName: string = this.userFirstName;
+     
+    const formValue = this.userInfo.value as UserInfo
+    const userName: string = formValue.userName
+    const userFirstName: string = formValue.firstName
+    const userLastName: string = formValue.lastName
+
     const regExUppercase: RegExp = /([A-Z])/g;
     const regExNumber: RegExp = /([0-9])/g;
     const regExString: RegExp = /([a-z])/g;
+    const FLnameCheck: RegExp = /[a-z-A-Z]/g;
 
-    const testReg: RegExp = /[a-z-A-Z]/g;
-
+    // check section
     const userNameStringcheck = userName.match(regExString);
     const userNameNumbercheck = userName.match(regExNumber);
     const userUppercaseCheck: boolean = regExUppercase.test(userName);
     const stringLength = userNameStringcheck?.length as number;
     const numberLength = userNameNumbercheck?.length as number;
     const whiteSpaceCheck: boolean = /\s/.test(userName);
-
+    
+    const firstNameCheck: boolean = FLnameCheck.test(userFirstName)
+    const lastNameCheck: boolean = FLnameCheck.test(userLastName)
     if (
       numberLength === 3 &&
       stringLength === 3 &&
@@ -50,8 +60,10 @@ export class SignUpComponent {
       whiteSpaceCheck === false
     ) {
       console.log('valid username');
-      if (this.userFirstName.length === 6) {
+      if (firstNameCheck && lastNameCheck === true) {
         console.log('valid firstname');
+      } else {
+        console.log('invalid firstname or lastname')
       }
     } else {
       console.log(
